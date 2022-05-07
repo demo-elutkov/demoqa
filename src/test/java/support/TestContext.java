a97c0044949211f7b122a4f85a5966ed9f7c13be
demo-elutkov <104608937+demo-elutkov@users.noreply.github.com>
2022-05-07 00:16:49 -0700

    Update TestContext.java

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -23,7 +23,7 @@ public class TestContext {
     }
 
     public static void initialize() {
-        initialize("chrome", "local", false);
+        initialize("firefox", "local", false);
     }
 
     public static void teardown() {
